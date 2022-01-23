b6a7fbbcb1571250a03aef78f84bbb6beeab3d3a
Evans Enonchong <citrudevmobile@gmail.com>
2022-01-23 15:01:52 +0100

    save chnages

--- web/routes/index.js
+++ web/routes/index.js
@@ -1,4 +1,9 @@
+const Auth = require('../libs/config-auth')
+
 module.exports = (app, io) => {
 
     app.get('/', (req, res) => { res.sendFile(path.join(__basedir + '/dist/index.html')) })
+    app.post('/login', Auth.login)
+    app.post('/signup', Auth.register)
+    app.post('/verify-auth', Auth.verifyAuthToken)
 }
