5eccf7885dcf2f2fc9e323cc174febdd1be04073
Evans Enonchong <citrudevmobile@gmail.com>
2022-01-24 17:07:41 +0100

    added emailing...

--- web/libs/emailTemplates.js
+++ web/libs/emailTemplates.js
@@ -2,7 +2,7 @@ module.exports = {
 
     verifyEmail: (token) => {
       return `<div>
-      <h1>Welcome to Best Crypto Bot Ever</h1>
+      <h1>Welcome to Best-Crypto-Bot-Ever</h1>
       <h3>Please follow this <a target="_blank" href="${process.env.DOMAIN_URL}/confirm-email-link/${token}">link</a> to verify your email.</h3>
       <br/>
       <p>Expires in 30 minutes</a>
@@ -11,12 +11,12 @@ module.exports = {
 
     resetPasswordEmail: (token) => {
       return `<div>
-      <h1>Welcome to Best Crypto Bot Ever</h1>
+      <h1>Welcome to Best-Crypto-Bot-Ever</h1>
       <h3>Please follow this <a target="_blank" href="${process.env.DOMAIN_URL}/reset-password/${token}">link</a> to reset your password.</h3>
       <br/>
       <p>Expires in 30 minutes</a>
       </div>`
     }
-    
+
   }
   
