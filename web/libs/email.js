110c9982a000209d2b65f481887314e43190816a
Evans Enonchong <citrudevmobile@gmail.com>
2022-01-24 16:40:32 +0100

    added emailing...

---
+++ web/libs/email.js
@@ -0,0 +1,19 @@
+const nodeMailer = require('nodemailer')
+
+const transporter = nodeMailer.createTransport({
+  host: 'smtp.zoho.com',
+  secure: true,
+  port: 465,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.Email_PASSWORD
+  }
+})
+
+module.exports = {
+  sendEmail: async function (mailOptions, callback) {
+    await transporter.sendMail(mailOptions, function (err, info) {
+      callback(err, info)
+    })
+  }
+}
---
+++ web/libs/emailTemplates.js
@@ -0,0 +1,22 @@
+module.exports = {
+
+    verifyEmail: (token) => {
+      return `<div>
+      <h1>Welcome to Best Crypto Bot Ever</h1>
+      <h3>Please follow this <a target="_blank" href="${process.env.DOMAIN_URL}/confirm-email-link/${token}">link</a> to verify your email.</h3>
+      <br/>
+      <p>Expires in 30 minutes</a>
+      </div>`
+    },
+
+    resetPasswordEmail: (token) => {
+      return `<div>
+      <h1>Welcome to Best Crypto Bot Ever</h1>
+      <h3>Please follow this <a target="_blank" href="${process.env.DOMAIN_URL}/reset-password/${token}">link</a> to reset your password.</h3>
+      <br/>
+      <p>Expires in 30 minutes</a>
+      </div>`
+    }
+    
+  }
+  
