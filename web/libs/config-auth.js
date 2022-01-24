6b30425ae14a2a4d1561d8046fb613299e9ff337
Evans Enonchong <citrudevmobile@gmail.com>
2022-01-24 16:38:23 +0100

    fixed docker-compose

--- web/libs/config-auth.js
+++ web/libs/config-auth.js
@@ -1,6 +1,8 @@
 const User = require('../models/user.model')
 const validator = require('email-validator')
 const jwt = require('jsonwebtoken')
+const email = require('./email')
+const emailTemplates = require('./emailTemplates')
 
 module.exports = {
 
@@ -88,6 +90,92 @@ module.exports = {
               .json({ auth: false, message: 'Failed to authenticate token.' })
           }
           res.status(200).json({ auth: true, message: 'Authenticated', user: decoded.id })
+        })
+      },
+
+      sendConfirmEmail: function (req, res) {
+        const token = req.body.token
+        jwt.verify(token, process.env.JWTsecret, function (err, decoded) {
+          if (err) {
+            return res
+              .status(401)
+              .json({ auth: false, message: 'Failed to authenticate token.' })
+          }
+          User.findOne({ _id: decoded.id }, function (err, user) {
+            if (err) return res.status(401).json({ sent: false })
+            if (!user) return res.status(200).json({ sent: false })
+            if (user) {
+              const emailToken = jwt.sign({ id: user._id }, process.env.JWTsecret, {
+                expiresIn: 1800
+              })
+              email.sendEmail({
+                from: `Best Crypto Bot Ever <info@bestcryptobotever.net>`,
+                to: user.email,
+                subject: 'Please confirm your email',
+                html: emailTemplates.verifyEmail(emailToken)
+              }, function (err) {
+                if (err) return res.status(401).json({ sent: false })
+                res.status(200).json({ sent: true })
+              })
+            }
+          })
+        })
+      },
+
+      activateUser: function (req, res) {
+        const token = req.headers['x-access-token']
+        if (!token) {
+          return res
+            .status(401)
+            .json({ activated: false, message: 'No token provided.' })
+        }
+        jwt.verify(token, process.env.JWTsecret, function (err, decoded) {
+          if (err) {
+            return res
+              .status(401)
+              .json({ activated: false, message: 'Failed to authenticate token.' })
+          }
+    
+          User.findOne({ _id: decoded.id }, function (err, user) {
+            if (err) return res.status(500).json({ reset: false, message: 'Internal server error while retriving user data' })
+            if (!user) return res.status(500).json({ activated: false, message: 'User not found' })
+            if (user) {
+              user.isActive = true
+              user.save(function (err, user) {
+                if (err) return res.status(500).json({ activated: false, message: 'Internal server error while saving changes to user.' })
+                return res.status(200).json({ activated: true })
+              })
+            }
+          })
+        })
+      },
+
+    
+      resetPassword: function (req, res) {
+        const token = req.headers['x-access-token']
+        if (!token) {
+          return res
+            .status(401)
+            .json({ reset: false, message: 'No token provided.' })
+        }
+        jwt.verify(token, process.env.JWTsecret, function (err, decoded) {
+          if (err) {
+            return res
+              .status(401)
+              .json({ reset: false, message: 'Failed to authenticate token.' })
+          }
+    
+          User.findOne({ _id: decoded.id }, function (err, user) {
+            if (err) return res.status(500).json({ reset: false, message: 'Internal server error while retriving user data' })
+            if (!user) return res.status(500).json({ reset: false, message: 'User not found' })
+            if (user) {
+              user.password = user.generateHash(req.body.password)
+              user.save(function (err, user) {
+                if (err) return res.status(500).json({ reset: false, message: 'Internal server error while saving changes to user.' })
+                return res.status(200).json({ reset: true })
+              })
+            }
+          })
         })
       },
 
--- web/views/components/home.js
+++ web/views/components/home.js
@@ -1,13 +1,10 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function App() {
-
-
-
-
-
-
-
+  
+    useEffect(()=>{
+        
+    }, [])
 
     return (
       <>
