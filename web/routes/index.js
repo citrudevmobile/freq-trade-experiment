ab8988bb32334c98a9cc12f152c2bfd216d2aba5
Evans Enonchong <citrudevmobile@gmail.com>
2022-01-27 14:35:09 +0100

    fixing rotes

--- web/routes/index.js
+++ web/routes/index.js
@@ -4,9 +4,9 @@ const path = require('path')
 module.exports = (app, io) => {
 
     app.get('/', (req, res) => { res.sendFile(path.join(__basedir + '/dist/index.html')) })
-    app.get('/dashboard', (req, res) => { res.sendFile(path.join(__basedir + '/dist/index.html')) })
     app.post('/login', Auth.login)
     app.post('/signup', Auth.register)
     app.post('/verify-auth', Auth.verifyAuthToken)
+    app.get('*', (req, res) => { res.sendFile(path.join(__basedir + '/dist/index.html')) })
     
 }
