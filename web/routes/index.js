866e6719e333d41de64eb416834bda488ed8e2b1
Evans Enonchong <citrudevmobile@gmail.com>
2022-01-27 14:16:45 +0100

    fixing dashboard

--- web/routes/index.js
+++ web/routes/index.js
@@ -3,6 +3,7 @@ const Auth = require('../libs/config-auth')
 module.exports = (app, io) => {
 
     app.get('/', (req, res) => { res.sendFile(path.join(__basedir + '/dist/index.html')) })
+    app.get('*', (req, res) => { res.sendFile(path.join(__basedir + '/dist/index.html')) })
     app.post('/login', Auth.login)
     app.post('/signup', Auth.register)
     app.post('/verify-auth', Auth.verifyAuthToken)
