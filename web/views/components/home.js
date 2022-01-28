a412e2ae27677f0d263e4015f4197e0e08ff517e
Evans Enonchong <citrudevmobile@gmail.com>
2022-01-28 17:35:09 +0100

    save changes

--- web/views/components/home.js
+++ web/views/components/home.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { NavLink, useLocation } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default function Home({authenticate}) {
 
