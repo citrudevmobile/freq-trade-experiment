b2160cdbf906fec358fa102ba1c6d4a8ecc5d1c6
Evans Enonchong <citrudevmobile@gmail.com>
2022-01-28 15:44:46 +0100

    save chnages

--- web/views/components/app.js
+++ web/views/components/app.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes, Navigate, useNavigate } from "react-router-dom";
 import axios from 'axios';
 
 
