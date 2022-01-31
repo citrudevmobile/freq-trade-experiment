805ccbc20e603786b3208d0e82a6e7fc2bcd66fb
Evans Enonchong <citrudevmobile@gmail.com>
2022-02-01 00:00:28 +0100

    save chnages

--- web/views/components/app.js
+++ web/views/components/app.js
@@ -16,6 +16,7 @@ const App = () => {
 
     let authenticate = function (cb) {
         let token = localStorage.token
+        console.log('authenticate called...')
         console.log(token)
         if (token) {
           axios({
