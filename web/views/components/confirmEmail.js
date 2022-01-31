96cd3460ed442e4f7a19bfd824a1f8f48d1e34b1
Evans Enonchong <citrudevmobile@gmail.com>
2022-01-31 20:03:16 +0100

    save chnages

--- web/views/components/confirmEmail.js
+++ web/views/components/confirmEmail.js
@@ -21,8 +21,8 @@ export default function Home({ authenticate }) {
     }
   
     useEffect(()=>{
-        authenticate()
-        sendConfirmEmail()
+        //authenticate()
+        //sendConfirmEmail()
     }, [])
 
     return (
