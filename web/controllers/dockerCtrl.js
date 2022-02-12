b2636c631dc105d8ab58778d430028f6cadba2df
citrudev <citrudevmobile@gmail.com>
2022-02-12 18:12:37 +0100

    save chnages

---
+++ web/controllers/dockerCtrl.js
@@ -0,0 +1,37 @@
+let Dockerode = require('dockerode');
+let DockerodeCompose = require('dockerode-compose');
+
+
+module.exports = {
+
+    startBot: async function (req, res) {
+        let docker = new Dockerode();
+        let compose = new DockerodeCompose(docker, '../../freqtrade/docker-compose.yml', 'helloworld');
+        try {
+            await compose.pull();
+            let state = await compose.up();
+            console.log(state);
+            res.status(200).json(state)
+        } catch (e) {
+            console.log(e)
+            res.status(500).json(e)
+        }
+    },
+
+    stopBot: async function (req, res) {
+        let docker = new Dockerode();
+        let compose = new DockerodeCompose(docker, '../../freqtrade/docker-compose.yml', 'helloworld');
+        try {
+            let state = await compose.down({ volumes: true });
+            console.log(state);
+            res.status(200).json(state)
+        } catch (e) {
+            console.log(e)
+            res.status(500).json(e)
+        }
+    },
+
+    restartBot: async function () {
+
+    }
+}
--- web/routes/index.js
+++ web/routes/index.js
@@ -1,13 +1,20 @@
 const Auth = require('../libs/config-auth')
 const path = require('path')
+const DockerCtrl = require('../controllers/dockerCtrl')
 
 module.exports = (app, io) => {
+
+    
 
     app.post('/login', Auth.login)
     app.post('/register', Auth.register)
     app.post('/verify', Auth.verifyAuthToken)
     app.post('/send-confirm-email', Auth.sendConfirmEmail)
     app.post('/confirm-email-link', Auth.activateUser)
+  
+    app.post('/start-bot', DockerCtrl.startBot)
+    app.post('/stop-bot', DockerCtrl.stopBot)
+   
     app.get('*', (req, res) => { res.sendFile(path.join(__basedir + '/dist/index.html')) })
     
 }
