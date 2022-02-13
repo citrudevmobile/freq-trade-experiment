cb3fa709a493ecb1ab7414f104896098763df529
citrudev <citrudevmobile@gmail.com>
2022-02-13 22:51:53 +0100

    save chnages

--- web/controllers/dockerCtrl.js
+++ web/controllers/dockerCtrl.js
@@ -20,7 +20,7 @@ module.exports = {
 
     stopBot: async function (req, res) {
         let docker = new Dockerode({host: '127.0.0.1', port: 3000})
-        let compose = new DockerodeCompose(docker, '../freqtrade/docker-compose.yml', 'helloworld')
+        let compose = new DockerodeCompose(docker, `${process.cwd()}/freqtrade/docker-compose.yml`, 'helloworld')
         try {
             let state = await compose.down({ volumes: true })
             console.log(state)
@@ -33,7 +33,7 @@ module.exports = {
 
     restartBot: async function () {
         let docker = new Dockerode({host: '127.0.0.1', port: 3000})
-        let compose = new DockerodeCompose(docker, '../freqtrade/docker-compose.yml', 'helloworld')
+        let compose = new DockerodeCompose(docker, `${process.cwd()}/freqtrade/docker-compose.yml`, 'helloworld')
         try {
             let state = await compose.down({ volumes: true })
             console.log(state)
