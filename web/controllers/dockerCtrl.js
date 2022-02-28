2a72253a97fa39d8e178a70ce10b50d07033f34b
citrudev <citrudevmobile@gmail.com>
2022-02-28 06:14:05 +0100

    save chnages

--- web/controllers/dockerCtrl.js
+++ web/controllers/dockerCtrl.js
@@ -72,7 +72,7 @@ module.exports = {
 
         let docker = new Dockerode();
         await docker.pull(opts.Image)
-        docker.run(opts.Image, [], process.stdout, {name: opts.name, Hostname: opts.name, Volumes: { '/freqtrade/user_data': {} }, ExposedPorts: { '8080/tcp': {} }, Cmd: 'freqtrade create-userdir --userdir user_data', HostConfig: { NetworkMode: 'freqtradenet', Binds: ['/root/trader_bot/web/freqtrade/user_data:/freqtrade/user_data']}}, function(err, data, container) {
+        docker.run(opts.Image, [], process.stdout, {OpenStdin: true, AttachStdin: true, name: opts.name, Hostname: opts.name, Volumes: { '/freqtrade/user_data': {} }, ExposedPorts: { '8080/tcp': {} }, Cmd: 'freqtrade create-userdir --userdir user_data', HostConfig: { NetworkMode: 'freqtradenet', Binds: ['/root/trader_bot/web/freqtrade/user_data:/freqtrade/user_data']}}, function(err, data, container) {
             if (err) return res.status(500).json({})
             containerId = container.id
             res.status(200).json({id: container.id})
