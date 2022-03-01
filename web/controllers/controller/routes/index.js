e7385576ada876f331e030b8c382eb5babfad2ec
citrudev <citrudevmobile@gmail.com>
2022-03-01 17:01:38 +0100

    save chnages

--- web/controllers/controller/routes/index.js
+++ web/controllers/controller/routes/index.js
@@ -1,5 +1,6 @@
 module.exports = (app, io) => { 
     app.get('/', (req, res) => {
+        console.log('controller works')
         res.status(200).json({message: 'hello this is a basic message'})
     })
 }
