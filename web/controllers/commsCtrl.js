423a0f3fef9503b2aa51a922a2593717d2329704
citrudev <citrudevmobile@gmail.com>
2022-03-02 11:59:06 +0100

    save chnages

--- web/controllers/commsCtrl.js
+++ web/controllers/commsCtrl.js
@@ -7,8 +7,8 @@ module.exports = {
             console.log(response.data);
             res.status(200).json({data: response.data})
           } catch (error) {
-            console.error(error);
-            res.status(500).json({response: error})
+            console.error('Unable to reach controller');
+            res.status(500).json({})
           }
     }
 }
