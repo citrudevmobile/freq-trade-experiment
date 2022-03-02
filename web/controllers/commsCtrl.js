771bc8fbab32515ea59a1d1bf865f0bde3c143bd
citrudev <citrudevmobile@gmail.com>
2022-03-02 11:47:58 +0100

    save chnages

--- web/controllers/commsCtrl.js
+++ web/controllers/commsCtrl.js
@@ -4,8 +4,8 @@ module.exports = {
     pingBot: async function (req, res) {
         try {
             const response = await axios.get('http://localhost:8080/');
-            console.log(response);
-            res.status(200).json({response: response})
+            console.log(response.data);
+            res.status(200).json({data: response.data})
           } catch (error) {
             console.error(error);
             res.status(500).json({response: error})
