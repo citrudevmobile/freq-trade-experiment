556bbc4b14a30876cf03edf5f8fb22c6becce9a8
citrudev <citrudevmobile@gmail.com>
2022-03-02 08:11:11 +0100

    save chnages

--- web/controllers/dockerCtrl.js
+++ web/controllers/dockerCtrl.js
@@ -122,7 +122,7 @@ module.exports = {
         */
         
         let docker = new Dockerode()
-        docker.run('controller:latest', [], process.stdout, function(err, data, container) {
+        docker.run('controller', [], process.stdout, function(err, data, container) {
             console.log(err)
             if (err) return res.status(500).json({})
             containerId = container.id
