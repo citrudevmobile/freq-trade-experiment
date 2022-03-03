40ff2180966ad8e130a63bba46de6f237a9fc6f0
citrudev <citrudevmobile@gmail.com>
2022-03-03 12:41:21 +0100

    save chnages

--- web/controllers/commsCtrl.js
+++ web/controllers/commsCtrl.js
@@ -8,11 +8,9 @@ module.exports = {
     pingCtrlBot: async function (req, res) {
         try {
             const response = await axios.get('http://localhost:8080/')
-            console.log(response.data);
-            res.status(200).json({data: response.data})
+            res.status(200).json(response.data)
           } catch (error) {
-            console.error('Unable to reach controller')
-            res.status(500).json({})
+            res.status(500).json({message: 'Unable to reach controller'})
           }
     },
 
