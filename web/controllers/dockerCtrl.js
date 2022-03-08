1ff51cf8cabde4860cc94c10f8e44a505bbd934d
citrudev <citrudevmobile@gmail.com>
2022-03-08 15:13:12 +0100

    save chnages

--- web/controllers/dockerCtrl.js
+++ web/controllers/dockerCtrl.js
@@ -91,7 +91,8 @@ module.exports = {
             HostConfig: { 
                 NetworkMode: 'freqtrade_network', 
             },
-            Cmd: [`ls /freqtrade/user_data`]
+            Cmd: [`ls /freqtrade/user_data`],
+            Entrypoint: [""]
         }
 
         //trade --logfile /freqtrade/user_data/logs/freqtrade.log --db-url sqlite:////freqtrade/user_data/tradesv3.sqlite --config /freqtrade/user_data/config.json --strategy SampleStrategy
