518cb97cffdc8b116ca9b55b4b05a2f5729b3717
citrudev <citrudevmobile@gmail.com>
2022-03-08 09:20:24 +0100

    save changes

--- web/controllers/dockerCtrl.js
+++ web/controllers/dockerCtrl.js
@@ -7,24 +7,51 @@ module.exports = {
         let ctrlCreateOptions =  {
             name: req.body.name, 
             Hostname: req.body.name, 
-            Image: 'tradebot',
+            Image: 'freqtradeorg/freqtrade:stable',
             Env: [`BOT_NAME=${req.body.name}`],
             ExposedPorts: { '8080/tcp': {} }, 
             HostConfig: { 
                 NetworkMode: 'freqtrade_network', 
-                Binds: [`${process.cwd()}/controllers/temptradebot:/usr/src/app`], 
             },
         }
-        try {
-            let docker = new Dockerode()
-            let freqtrade = await docker.getImage('freqtradeorg/freqtrade:stable')
-            let result = await freqtrade.inspect()
-            console.log(result)
-            res.status(200).json({})
-        } catch (e) {
-            console.log(e)
-            res.status(500).json({})
-        }
+
+        let docker = new Dockerode()
+        let container = null
+        let containers = await docker.listContainers()
+        containers = containers.filter( container => container.Names.includes(`/${req.body.name}`) )
+        if (!(containers.length > 0)) { 
+            try {
+                let networkName = ctrlCreateOptions.HostConfig.NetworkMode
+                let networks = await docker.listNetworks()
+                let network = networks.filter( network => network.Name == networkName )
+                if (!(network.length > 0)) {
+                    await docker.createNetwork({ 'Name': networkName, 'CheckDuplicate': true })
+                } 
+                try {
+                    container = await docker.createContainer(ctrlCreateOptions)
+                    await container.attach({
+                        stream: true,
+                        stdout: true,
+                        stderr: true
+                      }, function handler(err, stream) {
+                        //...
+                        container.modem.demuxStream(stream, process.stdout, process.stderr);
+                        //...
+                      });
+                    await container.start()
+                    res.status(200).json({id: container.id, name: req.body.name})
+                } catch(e) {
+                    console.log(e)
+                    res.status(500).json({})
+                }
+            } catch (e) {
+                console.log('Error occured while creating bot network...')
+                console.log(e)
+                res.status(500).json({message: 'Internal server error'})
+            }
+        } else {
+            res.status(200).json({id: containers[0].Id, name: ctrlCreateOptions.name})
+        }   
     },
 
     startCtrlBot: async function (req, res) {
@@ -60,15 +87,7 @@ module.exports = {
                 } 
                 try {
                     container = await docker.createContainer(ctrlCreateOptions)
-                    await container.attach({
-                        stream: true,
-                        stdout: true,
-                        stderr: true
-                      }, function handler(err, stream) {
-                        //...
-                        container.modem.demuxStream(stream, process.stdout, process.stderr);
-                        //...
-                      });
+                   
                     await container.start()
                     res.status(200).json({id: container.id, name: ctrlCreateOptions.name})
                 } catch(e) {
@@ -115,6 +134,15 @@ module.exports = {
                 } 
                 try {
                     container = await docker.createContainer(ctrlCreateOptions)
+                    await container.attach({
+                        stream: true,
+                        stdout: true,
+                        stderr: true
+                      }, function handler(err, stream) {
+                        //...
+                        container.modem.demuxStream(stream, process.stdout, process.stderr);
+                        //...
+                      });
                     await container.start()
                     res.status(200).json({id: container.id, name: req.body.name})
                 } catch(e) {
