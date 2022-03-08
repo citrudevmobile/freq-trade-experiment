a9026adbe35aa41d374ee8285d9f8b82e0ca5682
citrudev <citrudevmobile@gmail.com>
2022-03-08 14:36:25 +0100

    fixing bot

--- web/controllers/dockerCtrl.js
+++ web/controllers/dockerCtrl.js
@@ -91,7 +91,7 @@ module.exports = {
             HostConfig: { 
                 NetworkMode: 'freqtrade_network', 
             },
-            Cmd: `freqtrade --version`
+            Cmd: `freqtrade -V`
         }
 
         //trade --logfile /freqtrade/user_data/logs/freqtrade.log --db-url sqlite:////freqtrade/user_data/tradesv3.sqlite --config /freqtrade/user_data/config.json --strategy SampleStrategy
