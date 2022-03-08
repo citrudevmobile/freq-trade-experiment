bcc33f93810ba48cc00794da2be851b991b858cc
citrudev <citrudevmobile@gmail.com>
2022-03-08 20:10:03 +0100

    save changes

--- web/controllers/dockerCtrl.js
+++ web/controllers/dockerCtrl.js
@@ -105,7 +105,7 @@ module.exports = {
             ExposedPorts: { '8080/tcp': {} }, 
             HostConfig: { 
                 NetworkMode: 'freqtrade_network',
-                Binds: [`${process.cwd()}/freqtrade/freqtrade/user_data:/freqtrade/user_data`] 
+                Binds: [`${process.cwd()}/freqtrade/user_data:/freqtrade/user_data`] 
             },
             Entrypoint: ["freqtrade"],
             Cmd: [`trade`, `--config`, `/freqtrade/user_data/config.json`, `--logfile`, `/freqtrade/user_data/logs/freqtrade.log`, `--db-url`, `sqlite:////freqtrade/user_data/tradesv3.sqlite`, `--strategy`, `SampleStrategy`],
