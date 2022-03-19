121f81af63d1f14a3c385b38e72c53122dc9675e
citrudev <citrudevmobile@gmail.com>
2022-03-19 09:25:15 +0100

    save changes

--- web/views/components/dashboard.js
+++ web/views/components/dashboard.js
@@ -24,7 +24,6 @@ function Dashboard ({logout}) {
                     url: "/get-tradebots",
                     headers: { "x-access-token": token }
                 })
-                console.log(response.data)
                 setlistOfBots(response.data)
             } catch (error) {
                 console.log(error)
@@ -34,8 +33,61 @@ function Dashboard ({logout}) {
         }
     }
 
-    let startAndStop = async function (taskId) {
+    let deleteTradeBot = async function (taskId) {
+        let token = localStorage.getItem('token')
+            if (token) {
+                try {
+                     await axios({
+                        method: "post",
+                        url: "/delete-tradebot",
+                        headers: { "x-access-token": token },
+                        data: {taskId: taskId}
+                    })
+                    await getTradeBots()
+                } catch (error) {
+                    console.log(error)
+                }
+            } else {
+                navigate('/')
+            }
+    }
+
+    let startAndStop = async function (taskId, status) {
         console.log(taskId)
+        let token = localStorage.getItem('token')
+        if (token) {
+            if (status) {
+                try {
+                    await axios({
+                        method: "post",
+                        url: "/stop-tradebot",
+                        headers: { "x-access-token": token },
+                        data: {
+                            taskId: taskId
+                        }
+                    })
+                    await getTradeBots()
+                } catch (error) {
+                    console.log(error)
+                }
+            } else {
+                try {
+                    await axios({
+                        method: "post",
+                        url: "/start-tradebot",
+                        headers: { "x-access-token": token },
+                        data: {
+                            taskId: taskId
+                        }
+                    })
+                    await getTradeBots()
+                } catch (error) {
+                    console.log(error)
+                }
+            }
+        } else {
+            navigate('/')
+        }
     }
 
   useEffect(() => {
@@ -62,7 +114,7 @@ function Dashboard ({logout}) {
                 <div class="flex items-center justify-between">
                     <p tabindex="0" class="focus:outline-none text-base sm:text-lg md:text-xl lg:text-2xl font-bold leading-normal text-gray-800">Bots</p>
                     <div class="py-3 px-4 flex items-center text-sm font-medium leading-none text-gray-600 bg-gray-200 hover:bg-gray-300 cursor-pointer rounded">
-                        1
+                        {listOfBots.length}
                     </div>
                 </div>
             </div>
@@ -183,30 +235,47 @@ function Dashboard ({logout}) {
                                     <button class="py-3 px-3 mt-3 text-sm focus:outline-none leading-none text-red-700 bg-red-100 rounded">15</button>
                                 </td>
                                 <td class="pl-4">
-                                    <button class="focus:ring-2 focus:ring-offset-2 focus:ring-red-300 text-sm leading-none text-green-600 py-3 px-5 bg-gray-100 rounded hover:bg-gray-200 focus:outline-none">Active</button>
+                                    <button class="focus:ring-2 focus:ring-offset-2 focus:ring-red-300 text-sm leading-none text-green-600 py-3 px-5 bg-gray-100 rounded hover:bg-gray-200 focus:outline-none">{ val.status ? 'Running': 'Stopped' }</button>
                                 </td>
                                 <td class="pl-1 flex items-center justify-center">
-                                    <button onClick={() => {startAndStop(val.taskId)}} class="">
-                                        <i class="fas fa-play"></i>
+                                    <button onClick={() => {startAndStop(val.taskId, val.status)}} class="">
+                                        { val.status ? <i class="fas fa-stop"></i> : <i class="fas fa-play"></i> }
                                     </button>
                                 </td>
                                 <td>
                                     <div class="relative px-5 pt-2">
-                                        <button class="focus:ring-2 rounded-md focus:outline-none" onclick="dropdownFunction(this)" role="button" aria-label="option">
+
+                                        <button data-modal-toggle="large-modal" class="focus:ring-2 rounded-md focus:outline-none" onclick="dropdownFunction(this)" role="button" aria-label="option">
                                             <svg class="dropbtn" onclick="dropdownFunction(this)" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
                                                 <path d="M4.16667 10.8332C4.62691 10.8332 5 10.4601 5 9.99984C5 9.5396 4.62691 9.1665 4.16667 9.1665C3.70643 9.1665 3.33334 9.5396 3.33334 9.99984C3.33334 10.4601 3.70643 10.8332 4.16667 10.8332Z" stroke="#9CA3AF" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></path>
                                                 <path d="M10 10.8332C10.4602 10.8332 10.8333 10.4601 10.8333 9.99984C10.8333 9.5396 10.4602 9.1665 10 9.1665C9.53976 9.1665 9.16666 9.5396 9.16666 9.99984C9.16666 10.4601 9.53976 10.8332 10 10.8332Z" stroke="#9CA3AF" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></path>
                                                 <path d="M15.8333 10.8332C16.2936 10.8332 16.6667 10.4601 16.6667 9.99984C16.6667 9.5396 16.2936 9.1665 15.8333 9.1665C15.3731 9.1665 15 9.5396 15 9.99984C15 10.4601 15.3731 10.8332 15.8333 10.8332Z" stroke="#9CA3AF" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"></path>
                                             </svg>
                                         </button>
-                                        <div class="dropdown-content bg-white shadow w-24 absolute z-30 right-0 mr-6 hidden">
-                                            <div tabindex="0" class="focus:outline-none focus:text-indigo-600 text-xs w-full hover:bg-indigo-700 py-4 px-4 cursor-pointer hover:text-white">
-                                                <p>Edit</p>
-                                            </div>
-                                            <div tabindex="0" class="focus:outline-none focus:text-indigo-600 text-xs w-full hover:bg-indigo-700 py-4 px-4 cursor-pointer hover:text-white">
-                                                <p>Delete</p>
+
+                                        <div class="hidden overflow-y-auto overflow-x-hidden fixed right-0 left-0 top-4 z-50 justify-center items-center md:inset-0 h-modal sm:h-full" id="large-modal">
+                                            <div class="relative px-4 w-full max-w-4xl h-full md:h-auto">
+                                            
+                                                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                                
+                                                    <div class="flex justify-between items-center p-5 rounded-t border-b dark:border-gray-600">
+                                                        <h3 class="text-xl font-medium text-gray-900 dark:text-white">
+                                                            {val.name}
+                                                        </h3>
+                                                        <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="large-modal">
+                                                            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>  
+                                                        </button>
+                                                    </div>
+                                                    
+                                                    <div class="p-6 space-y-6">
+                                                         <button onClick={() => {deleteTradeBot(val.taskId)}}  data-modal-toggle="large-modal" type="button" class="text-white bg-red-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Delete Bot</button>
+                                                    </div>
+                                                  
+                                                </div>
                                             </div>
                                         </div>
+
+
                                     </div>
                                 </td>
                             </tr>
