264d19387364bb95d57e915084fb24f5595632bb
citrudev <citrudevmobile@gmail.com>
2022-04-04 13:58:21 +0100

    save chnages

--- web/views/components/create-bot.js
+++ web/views/components/create-bot.js
@@ -71,8 +71,15 @@ function Dashboard ({logout}) {
               </div>
             </div>
 
+            <div>
+              <label for="toggle-example-checked" class="flex relative items-center mb-4 cursor-pointer">
+                <input type="checkbox" id="toggle-example-checked" class="sr-only" checked/>
+                <div class="w-11 h-6 bg-gray-200 rounded-full border border-gray-200 toggle-bg dark:bg-gray-700 dark:border-gray-600"></div>
+                <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Paper Trade</span>
+              </label>
+            </div>
             
-              <div class="mt-4">
+              <div class="mt-6">
                 <div class="grid xl:grid-cols-2 xl:gap-6">
                   <div class="relative z-0 mb-6 w-full group">
                       <input type="text" name="floating_first_name" id="floating_first_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
