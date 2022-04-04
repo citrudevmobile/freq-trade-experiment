72980db484967c30216713eddf46ad36c54e978f
citrudev <citrudevmobile@gmail.com>
2022-04-04 14:00:36 +0100

    save chnages

--- web/views/components/create-bot.js
+++ web/views/components/create-bot.js
@@ -71,7 +71,7 @@ function Dashboard ({logout}) {
               </div>
             </div>
 
-            <div>
+            <div class="mt-6">
               <label for="toggle-example-checked" class="flex relative items-center mb-4 cursor-pointer">
                 <input type="checkbox" id="toggle-example-checked" class="sr-only" checked/>
                 <div class="w-11 h-6 bg-gray-200 rounded-full border border-gray-200 toggle-bg dark:bg-gray-700 dark:border-gray-600"></div>
