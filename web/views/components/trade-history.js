23931511029448ff90da054eeb24362875d63006
Citrudev <citrudevmobile@gmail.com>
2022-04-19 14:49:42 +0100

    save changes

--- web/views/components/trade-history.js
+++ web/views/components/trade-history.js
@@ -2,17 +2,14 @@ import React, { useState, useEffect } from "react";
 import { NavLink, Navigate, useNavigate } from "react-router-dom";
 import Sidebar from './dashboard/Sidebar'
 import Header from './dashboard/Header';
-import WelcomeBanner from './dashboard/WelcomeBanner';
-import DashboardCard05 from './dashboard/Dashboard05';
-import swap from '../images/swap.svg';
-import clock from '../images/clock1.svg';
 import axios from 'axios';
 
 
 function Dashboard ({logout}) {
 
   const [sidebarOpen, setSidebarOpen] = useState(true);
-  const [listOfBots, setlistOfBots] = useState([]);
+  const [allTrades, setAllTrades] = useState([]);
+
   let navigate = useNavigate()
   
   let getTradeBots = async function () {
@@ -24,75 +21,36 @@ function Dashboard ({logout}) {
                     url: "/get-tradebots",
                     headers: { "x-access-token": token }
                 })
-                setlistOfBots(response.data)
+                let allTradeBots = response.data
+                let trades = []
+                for (let bot of allTradeBots) {
+                    bot.trades.map(trade => {
+                        trade = bot.name
+                        return trade 
+                    })
+                    trades = trades.concat(bot.trades)
+                }
+                trades = trades.sort(function(a, b) {
+                    return new Date(b.open_date) - new Date(a.open_date);
+                })
+                setAllTrades(trades)
             } catch (error) {
                 console.log(error)
             }
         } else {
-            //navigate('/')
+            navigate('/')
         }
     }
 
-    let deleteTradeBot = async function (taskId) {
-        let token = localStorage.getItem('token')
-            if (token) {
-                try {
-                     await axios({
-                        method: "post",
-                        url: "/delete-tradebot",
-                        headers: { "x-access-token": token },
-                        data: {taskId: taskId}
-                    })
-                    await getTradeBots()
-                } catch (error) {
-                    console.log(error)
-                }
-            } else {
-                //navigate('/')
-            }
-    }
-
-    let startAndStop = async function (taskId, status) {
-        console.log(taskId)
-        let token = localStorage.getItem('token')
-        if (token) {
-            if (status) {
-                try {
-                    await axios({
-                        method: "post",
-                        url: "/stop-tradebot",
-                        headers: { "x-access-token": token },
-                        data: {
-                            taskId: taskId
-                        }
-                    })
-                    await getTradeBots()
-                } catch (error) {
-                    console.log(error)
-                }
-            } else {
-                try {
-                    await axios({
-                        method: "post",
-                        url: "/start-tradebot",
-                        headers: { "x-access-token": token },
-                        data: {
-                            taskId: taskId
-                        }
-                    })
-                    await getTradeBots()
-                } catch (error) {
-                    console.log(error)
-                }
-            }
-        } else {
-            //navigate('/')
-        }
-    }
+    
 
   useEffect(() => {
     getTradeBots()
   }, [])
+
+  useEffect(() => {
+    console.log(allTrades)
+  }, [allTrades])
 
   return (
     <div className="flex h-screen overflow-hidden">
@@ -110,33 +68,21 @@ function Dashboard ({logout}) {
 
             <div class="sm:px-6 w-full">
 
-<ol class="relative border-l border-gray-200 dark:border-gray-700">                  
-    <li class="mb-10 ml-6">            
-        <span class="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-            <svg class="w-3 h-3 text-blue-600 dark:text-blue-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
-        </span>
-        <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">Flowbite Application UI v2.0.0 <span class="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">Latest</span></h3>
-        <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Released on January 13th, 2022</time>
-        <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.</p>
-        <a href="#" class="inline-flex items-center py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"><svg class="mr-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3.586l-1.293-1.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V8z" clip-rule="evenodd"></path></svg> Download ZIP</a>
-    </li>
-    <li class="mb-10 ml-6">
-        <span class="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-            <svg class="w-3 h-3 text-blue-600 dark:text-blue-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
-        </span>
-        <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">Flowbite Figma v1.3.0</h3>
-        <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Released on December 7th, 2021</time>
-        <p class="text-base font-normal text-gray-500 dark:text-gray-400">All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.</p>
-    </li>
-    <li class="mb-10 ml-6">
-        <span class="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-            <svg class="w-3 h-3 text-blue-600 dark:text-blue-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
-        </span>
-        <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">Flowbite Library v1.2.2</h3>
-        <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Released on December 2nd, 2021</time>
-        <p class="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements built on top of Tailwind CSS.</p>
-    </li>
-</ol>
+                <ol class="relative border-l border-gray-200 dark:border-gray-700">  
+                    {allTrades.map((val, index) => {
+                    return (
+                        <li class="mb-10 ml-6">            
+                            <span class="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                                <svg class="w-3 h-3 text-blue-600 dark:text-blue-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                            </span>
+                            <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">Flowbite Application UI v2.0.0 <span class="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">Latest</span></h3>
+                            <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Released on January 13th, 2022</time>
+                            <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce & Marketing pages.</p>
+                            <a href="#" class="inline-flex items-center py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"><svg class="mr-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3.586l-1.293-1.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V8z" clip-rule="evenodd"></path></svg> Download ZIP</a>
+                        </li>                                          
+                    )})}
+                </ol>
+
             </div>
     
           </div>
