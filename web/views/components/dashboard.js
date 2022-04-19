d021033c908b20cb609e3314f3a093c887a1f159
Citrudev <citrudevmobile@gmail.com>
2022-04-19 02:39:21 +0100

    save chnages

--- web/views/components/dashboard.js
+++ web/views/components/dashboard.js
@@ -30,6 +30,7 @@ function Dashboard ({logout}) {
   const [modalIsOpen, setIsOpen] = React.useState(false);
 
   function openModal() {
+    console.log('open modal called...')
     setIsOpen(true);
   }
 
@@ -123,6 +124,46 @@ function Dashboard ({logout}) {
 
   return (
     <div className="flex h-screen overflow-hidden">
+                                                <Modal
+                                                    isOpen={modalIsOpen}
+                                                    onAfterOpen={afterOpenModal}
+                                                    onRequestClose={closeModal}
+                                                    style={customStyles}
+                                                    contentLabel="Example Modal"
+                                                >
+                                                   
+                                                    
+    <div>
+    <div class="w-full max-w-4xl h-full h-auto">
+       
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+          
+            <div class="flex justify-between items-start p-5 rounded-t border-b dark:border-gray-600">
+                <h3 class="text-xl font-semibold text-gray-900 lg:text-2xl dark:text-white">
+                    Terms of Service
+                </h3>
+                <button onClick={closeModal}  type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="defaultModal">
+                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>  
+                </button>
+            </div>
+            
+            <div class="p-6 space-y-6">
+                <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                    With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
+                </p>
+                <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                    The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
+                </p>
+            </div>
+           
+            <div class="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
+                <button onClick={closeModal}  data-modal-toggle="defaultModal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
+                <button onClick={closeModal} data-modal-toggle="defaultModal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
+            </div>
+        </div>
+    </div>
+</div>
+</Modal>
 
       {/* Sidebar */}
       <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
@@ -143,6 +184,7 @@ function Dashboard ({logout}) {
                     <div class="py-3 px-4 flex items-center text-sm font-medium leading-none text-gray-600 bg-gray-200 hover:bg-gray-300 cursor-pointer rounded">
                         {listOfBots.length}
                     </div>
+                    
                 </div>
             </div>
             <div class="bg-white py-4 md:py-7 px-4 md:px-8 xl:px-10">
@@ -281,24 +323,7 @@ function Dashboard ({logout}) {
                                                 
                                         </button>
 
-                                                <Modal
-                                                    isOpen={modalIsOpen}
-                                                    onAfterOpen={afterOpenModal}
-                                                    onRequestClose={closeModal}
-                                                    style={customStyles}
-                                                    contentLabel="Example Modal"
-                                                >
-                                                    <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
-                                                    <button onClick={closeModal}>close</button>
-                                                    <div>I am a modal</div>
-                                                    <form>
-                                                    <input />
-                                                    <button>tab navigation</button>
-                                                    <button>stays</button>
-                                                    <button>inside</button>
-                                                    <button>the modal</button>
-                                                    </form>
-                                                </Modal>
+                                                
 
                                     </div>
                                 </td>
