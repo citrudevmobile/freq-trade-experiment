17d70789e558231a28f3798d1bf4393976aed249
Citrudev <citrudevmobile@gmail.com>
2022-04-20 17:49:29 +0100

    save changes

--- web/views/components/trade-history.js
+++ web/views/components/trade-history.js
@@ -5,15 +5,15 @@ import Header from './dashboard/Header';
 import axios from 'axios';
 
 
-function Dashboard ({logout}) {
+function Dashboard({ logout }) {
 
-  const [sidebarOpen, setSidebarOpen] = useState(true);
-  const [allTrades, setAllTrades] = useState([]);
+    const [sidebarOpen, setSidebarOpen] = useState(true);
+    const [allTrades, setAllTrades] = useState([]);
 
-  let navigate = useNavigate()
-  
-  let getTradeBots = async function () {
-    let token = localStorage.getItem('token')
+    let navigate = useNavigate()
+
+    let getTradeBots = async function () {
+        let token = localStorage.getItem('token')
         if (token) {
             try {
                 let response = await axios({
@@ -26,11 +26,11 @@ function Dashboard ({logout}) {
                 for (let bot of allTradeBots) {
                     let _trades = bot.trades.map(_trade => {
                         _trade.bot_name = bot.name
-                        return _trade 
+                        return _trade
                     })
                     trades = trades.concat(_trades)
                 }
-                trades = trades.sort(function(a, b) {
+                trades = trades.sort(function (a, b) {
                     return new Date(b.open_date) - new Date(a.open_date);
                 })
                 console.log(trades)
@@ -43,112 +43,126 @@ function Dashboard ({logout}) {
         }
     }
 
-    
 
-  useEffect(() => {
-    getTradeBots()
-  }, [])
 
-  useEffect(() => {
-    console.log(allTrades)
-  }, [allTrades])
+    useEffect(() => {
+        getTradeBots()
+    }, [])
 
-  return (
-    <div className="flex h-screen overflow-hidden">
+    useEffect(() => {
+        console.log(allTrades)
+    }, [allTrades])
 
-      {/* Sidebar */}
-      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+    return (
+        <div className="flex h-screen overflow-hidden">
 
-      {/* Content area */}
-      <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+            {/* Sidebar */}
+            <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
-        {/*  Site header */}
-        <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} logout={logout} />
-        <main>
-          <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+            {/* Content area */}
+            <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
 
-            <div class="sm:px-6 w-full">
+                {/*  Site header */}
+                <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} logout={logout} />
+                <main>
+                    <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
 
-                <ol class="relative border-l border-gray-200 dark:border-gray-700">  
-                    {allTrades.map((val, index) => {
-                    return (
-                        <li class="mb-10 ml-6">            
-                            <span class="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-                                <svg class="w-3 h-3 text-blue-600 dark:text-blue-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
-                            </span>
-                            <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">{val.bot_name}<span class="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">Trade Open: {new Date(val.open_date).toLocaleString('en-US')}</span></h3>
-                                
-                                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                                        <tr>
-                                            <th scope="col" class="px-4 py-3">
-                                                TRADE TYPE
-                                            </th>
-                                            <th scope="col" class="px-4 py-3">
-                                                Exchange
-                                            </th>
-                                            <th scope="col" class="px-4 py-3">
-                                                PAIR
-                                            </th>
-                                            <th scope="col" class="px-4 py-3">
-                                                STAKE AMOUNT
-                                            </th>
-                                            <th scope="col" class="px-4 py-3">
-                                                TRADED AMOUNT
-                                            </th>
-                                            <th scope="col" class="px-4 py-3">
-                                                OPEN RATE
-                                            </th>
-                                            <th scope="col" class="px-4 py-3">
-                                                PROFIT
-                                            </th>
-                                            <th scope="col" class="px-4 py-3">
-                                                PROFIT RATIO
-                                            </th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr class="bg-white dark:bg-gray-800">
-                                            <td scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
-                                            {val['buy_tag'] == undefined ? 'Sell' : 'Buy'}
-                                            </td>
-                                            <td class="px-4 py-4">
-                                            {val.exchange}
-                                            </td>
-                                            <td class="px-4 py-4">
-                                            {val.pair}
-                                            </td>
-                                            <td class="px-4 py-4">
-                                            {val.stake_amount} {val.stake_currency}
-                                            </td>
-                                            <td class="px-4 py-4">
-                                            {val.amount} {val.base_currency}
-                                            </td>
-                                            <td class="px-4 py-4">
-                                            {val['open_rate']}
-                                            </td>
-                                            <td class="px-4 py-4">
-                                            {val['profit_amount'] == undefined ? 'N/A' : val['profit_amount']}
-                                            </td>
-                                            <td class="px-4 py-4">
-                                            {val['profit_ratio'] == undefined ? 'N/A' : val['profit_ratio']}
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                        </li>                                          
-                    )})}
-                </ol>
+                        <div class="sm:px-6 w-full">
+
+                            <div class="flex items-center space-x-2">
+                                <a class="rounded-full focus:outline-none focus:ring-2  focus:bg-indigo-50 focus:ring-indigo-800" href=" javascript:void(0)">
+                                    <div class="py-2 px-8 bg-indigo-50 text-indigo-700 rounded-full">
+                                        <p>All Trades: <span class="text-green-700 mx-1">{allTrades.length}</span></p>
+                                    </div>
+                                </a>
+                                <a class="rounded-full focus:outline-none focus:ring-2 focus:bg-indigo-50 focus:ring-indigo-800" href="javascript:void(0)">
+                                    <div class="py-2 px-8 bg-indigo-50 text-indigo-700 rounded-full">
+                                        <p>Total Value: <span class="text-black mx-1">$1000</span></p>
+                                    </div>
+                                </a>
+                            </div>
+
+                            <ol class="relative border-l border-gray-200 dark:border-gray-700">
+                                {allTrades.map((val, index) => {
+                                    return (
+                                        <li class="mb-10 ml-6">
+                                            <span class="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                                                <svg class="w-3 h-3 text-blue-600 dark:text-blue-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                                            </span>
+                                            <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">{val.bot_name}<span class="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">Trade Open: {new Date(val.open_date).toLocaleString('en-US')}</span></h3>
+
+                                            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                                                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                                    <tr>
+                                                        <th scope="col" class="px-4 py-3">
+                                                            TRADE TYPE
+                                                        </th>
+                                                        <th scope="col" class="px-4 py-3">
+                                                            Exchange
+                                                        </th>
+                                                        <th scope="col" class="px-4 py-3">
+                                                            PAIR
+                                                        </th>
+                                                        <th scope="col" class="px-4 py-3">
+                                                            STAKE AMOUNT
+                                                        </th>
+                                                        <th scope="col" class="px-4 py-3">
+                                                            TRADED AMOUNT
+                                                        </th>
+                                                        <th scope="col" class="px-4 py-3">
+                                                            OPEN RATE
+                                                        </th>
+                                                        <th scope="col" class="px-4 py-3">
+                                                            PROFIT
+                                                        </th>
+                                                        <th scope="col" class="px-4 py-3">
+                                                            PROFIT RATIO
+                                                        </th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr class="bg-white dark:bg-gray-800">
+                                                        <td scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                                                            {val['buy_tag'] == undefined ? 'Sell' : 'Buy'}
+                                                        </td>
+                                                        <td class="px-4 py-4">
+                                                            {val.exchange}
+                                                        </td>
+                                                        <td class="px-4 py-4">
+                                                            {val.pair}
+                                                        </td>
+                                                        <td class="px-4 py-4">
+                                                            {val.stake_amount} {val.stake_currency}
+                                                        </td>
+                                                        <td class="px-4 py-4">
+                                                            {val.amount} {val.base_currency}
+                                                        </td>
+                                                        <td class="px-4 py-4">
+                                                            {val['open_rate']}
+                                                        </td>
+                                                        <td class="px-4 py-4">
+                                                            {val['profit_amount'] == undefined ? 'N/A' : val['profit_amount']}
+                                                        </td>
+                                                        <td class="px-4 py-4">
+                                                            {val['profit_ratio'] == undefined ? 'N/A' : val['profit_ratio']}
+                                                        </td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </li>
+                                    )
+                                })}
+                            </ol>
+
+                        </div>
+
+                    </div>
+                </main>
 
             </div>
-    
-          </div>
-        </main>
+        </div>
+    )
 
-      </div>
-    </div>
-  )
+}
 
-  }
-
-  export default Dashboard
+export default Dashboard
