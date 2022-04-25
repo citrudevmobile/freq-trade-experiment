a262323c1e3ba4f28c9f85487343ee07916bb890
Citrudev <citrudevmobile@gmail.com>
2022-04-25 07:57:40 +0100

    save changes

--- web/views/components/trade-history.js
+++ web/views/components/trade-history.js
@@ -147,10 +147,10 @@ function Dashboard({ logout }) {
                                                             {val['profit_ratio'] == undefined ? Number(val['open_rate']) : Number(val['limit'])}
                                                         </td>
                                                         <td className={"px-4 py-4 " + (val['gain'] == 'profit'? 'text-green-400' : 'text-red-400')}>
-                                                            {val['profit_amount'] == undefined ? 'N/A' : (val['gain'] == 'profit'? '+':'-')+Number(val['profit_amount']).toFixed(2)}
+                                                            {val['profit_amount'] == undefined ? 'N/A' : Number(val['profit_amount']).toFixed(2)}
                                                         </td>
                                                         <td className={"px-4 py-4 " + (val['gain'] == 'profit'? 'text-green-400' : 'text-red-400')}>
-                                                            {val['profit_ratio'] == undefined ? 'N/A' : (val['gain'] == 'profit'? '+':'-')+Number(val['profit_ratio']).toFixed(2)}
+                                                            {val['profit_ratio'] == undefined ? 'N/A' : Number(val['profit_ratio']).toFixed(2)}
                                                         </td>
                                                     </tr>
                                                 </tbody>
