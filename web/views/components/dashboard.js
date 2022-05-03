9648e79301f2e1ef97df045ef051f083f651e152
Citrudev <citrudevmobile@gmail.com>
2022-05-03 12:29:44 +0100

    save changes

--- web/views/components/dashboard.js
+++ web/views/components/dashboard.js
@@ -319,7 +319,7 @@ function Dashboard({ logout }) {
                                                             <div class="flex items-center">
                                                                 <p class="text-sm leading-none text-green-600 ml-2">{val.trades.filter(function (_trade) { return !(_trade['close_date'] == undefined)}).sort(function (a, b) {
                                                                         return new Date(b.open_date) - new Date(a.open_date);
-                                                                    })[0]['open_rate']}
+                                                                    })[0]['open_rate'] || 'N/A'}
                                                                 </p>
                                                             </div>
                                                         </td>
