38393c43d8c5822d028614d94376c5d4996c3299
Citrudev <citrudevmobile@gmail.com>
2022-05-03 09:43:07 +0100

    save chnages

--- web/views/components/dashboard.js
+++ web/views/components/dashboard.js
@@ -27,6 +27,7 @@ function Dashboard({ logout }) {
     const [modalInfo, setModalInfo] = useState({})
     const [totalTrade, setTotalTrade] = useState(0)
     const [totalProfit, setTotalProfit] = useState(0)
+    const [totalProfitRatio, setTotalProfitRatio] = useState(0)
     const [modalIsOpen, setIsOpen] = React.useState(false)
 
     let navigate = useNavigate()
@@ -74,11 +75,16 @@ function Dashboard({ logout }) {
                 let totalTrades = closedTrades.reduce( (accumulator, val ) => {
                    return accumulator + Number(val['profit_amount'])
                 }, 0);
+
+                let totalTradesRatio = closedTrades.reduce( (accumulator, val ) => {
+                    return accumulator + Number(val['profit_ratio'])
+                 }, 0);
                 
 
                 setTotalProfit(totalTrades)
                 setTotalTrade(trades.length)
                 setlistOfBots(allTradeBots)
+                setTotalProfitRatio(totalTradesRatio)
 
             } catch (error) {
                 console.log(error)
@@ -225,7 +231,7 @@ function Dashboard({ logout }) {
                                     <div class="flex items-center space-x-2">
                                         <a class="rounded-full focus:outline-none focus:ring-2  focus:bg-indigo-50 focus:ring-indigo-800" href=" javascript:void(0)">
                                             <div class="py-2 px-8 bg-indigo-50 text-indigo-700 rounded-full">
-                                                <p>Total Profits: <span class="text-black mx-1">${totalProfit}</span><span class="text-green-700 mx-1">(+6.16%)</span></p>
+                                                <p>Total Profits: <span class="text-black mx-1">${totalProfit}</span><span class="text-green-700 mx-1">({totalProfitRatio * 100}%)</span></p>
                                             </div>
                                         </a>
                                         <a class="rounded-full focus:outline-none focus:ring-2 focus:bg-indigo-50 focus:ring-indigo-800" href="javascript:void(0)">
