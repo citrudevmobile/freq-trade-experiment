110390c2496b0fb9f160f9f0b550a84f52f7acca
Citrudev <citrudevmobile@gmail.com>
2022-05-03 03:10:21 +0100

    save changes

--- web/views/components/dashboard.js
+++ web/views/components/dashboard.js
@@ -21,16 +21,19 @@ const customStyles = {
 }
 
 function Dashboard({ logout }) {
+    let subtitle;
 
     const [sidebarOpen, setSidebarOpen] = useState(true);
     const [listOfBots, setlistOfBots] = useState([]);
     const [modalInfo, setModalInfo] = useState({})
-    const [totalCapital, setTotalCapital] = useState(0)
+    const [totalTrade, setTotalTrade] = useState(0)
     const [totalProfit, setTotalProfit] = useState(0)
+    const [modalIsOpen, setIsOpen] = React.useState(false)
+
     let navigate = useNavigate()
 
-    let subtitle;
-    const [modalIsOpen, setIsOpen] = React.useState(false);
+    
+    
 
     function openModal(val) {
         console.log('open modal called...')
@@ -43,11 +46,12 @@ function Dashboard({ logout }) {
         subtitle.style.color = '#f00';
     }
 
-    function closeModal() {
+    function closeModal () {
         setIsOpen(false);
     }
 
     let getTradeBots = async function () {
+
         let token = localStorage.getItem('token')
         if (token) {
             try {
@@ -59,17 +63,15 @@ function Dashboard({ logout }) {
                 let allBots = response.data
                 let totalCapital = 0
                 setlistOfBots(allBots)
-                allBots.forEach(function (bot) {
-                    totalCapital += Number(bot.recipe.available_capital)
-                })
                 console.log(allBots)
-                setTotalCapital(totalCapital)
+
             } catch (error) {
                 console.log(error)
             }
         } else {
             navigate('/')
         }
+        
     }
 
     let deleteTradeBot = async function (taskId) {
@@ -150,8 +152,8 @@ function Dashboard({ logout }) {
 
                             <div class="flex justify-between items-start p-5 rounded-t border-b dark:border-gray-600">
                                 <h3 class="text-xl flex items-center font-semibold text-gray-900 lg:text-2xl dark:text-white">
-                                {modalInfo.name}
-                                <p class="bg-blue-700 ml-4 text-white text-sm font-semibold inline-flex items-center p-1.5 rounded">Bot</p>
+                                    {modalInfo.name}
+                                    <p class="bg-blue-700 ml-4 text-white text-sm font-semibold inline-flex items-center p-1.5 rounded">Bot</p>
                                 </h3>
                                 <button onClick={closeModal} type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="defaultModal">
                                     <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
@@ -168,11 +170,11 @@ function Dashboard({ logout }) {
                             </div>
 
                             <div class="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
-                                <button onClick={()=>{
+                                <button onClick={() => {
                                     deleteTradeBot(modalInfo.taskId)
-                                    setTimeout(()=>{
+                                    setTimeout(() => {
                                         closeModal()
-                                    },3000)
+                                    }, 3000)
                                 }} data-modal-toggle="defaultModal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Delete Bot</button>
                                 <button onClick={closeModal} data-modal-toggle="defaultModal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
                             </div>
@@ -208,12 +210,12 @@ function Dashboard({ logout }) {
                                     <div class="flex items-center space-x-2">
                                         <a class="rounded-full focus:outline-none focus:ring-2  focus:bg-indigo-50 focus:ring-indigo-800" href=" javascript:void(0)">
                                             <div class="py-2 px-8 bg-indigo-50 text-indigo-700 rounded-full">
-                                                <p>Total Bot Profits: <span class="text-black mx-1">${totalProfit}</span><span class="text-green-700 mx-1">(+6.16%)</span></p>
+                                                <p>Total Profits: <span class="text-black mx-1">${totalProfit}</span><span class="text-green-700 mx-1">(+6.16%)</span></p>
                                             </div>
                                         </a>
                                         <a class="rounded-full focus:outline-none focus:ring-2 focus:bg-indigo-50 focus:ring-indigo-800" href="javascript:void(0)">
                                             <div class="py-2 px-8 bg-indigo-50 text-indigo-700 rounded-full">
-                                                <p>Total Capital: <span class="text-black mx-1">${totalCapital}</span></p>
+                                                <p>Total Trades: <span class="text-black mx-1">${totalTrade}</span></p>
                                             </div>
                                         </a>
                                     </div>
@@ -322,19 +324,19 @@ function Dashboard({ logout }) {
                                                         <td class="pl-4">
                                                             <button class="focus:ring-2 focus:ring-offset-2 focus:ring-red-300 text-sm leading-none text-green-600 py-3 px-5 bg-gray-100 rounded hover:bg-gray-200 focus:outline-none">
                                                                 {val.status ? 'Running' : 'Stopped'}
-                                                                <span className={ val.status == undefined ? '' : 'hidden' }>
+                                                                <span className={val.status == undefined ? '' : 'hidden'}>
                                                                     {<svg role="status" class="inline mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-yellow-400" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-                                                                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                                                                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                                                                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
                                                                     </svg>}
                                                                 </span>
                                                             </button>
                                                         </td>
                                                         <td class="pl-1 flex items-center justify-center">
                                                             <button onClick={() => { startAndStop(val.taskId, val.status) }} class="">
-                                                                <span className={ val.status ? 'hidden' : '' }>{ <i class="fas fa-play"></i> }</span>
-                                                                
-                                                                <span className={ val.status ? '' : 'hidden' }>{ <i class="fas fa-stop"></i> }</span>
+                                                                <span className={val.status ? 'hidden' : ''}>{<i class="fas fa-play"></i>}</span>
+
+                                                                <span className={val.status ? '' : 'hidden'}>{<i class="fas fa-stop"></i>}</span>
                                                             </button>
                                                         </td>
                                                         <td>
